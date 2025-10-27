075753b2d3852e7cffffd619618f73c52bca5187
erudit <philaoue@gmail.com>
2025-10-27 13:02:27 +0100

    create un ping.ts for test purpose

---
+++ src/commands/ping.ts
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder, ApplicationCommand } = require("discord.js");
+// SlashCommandBuilder est une classe utilitaire qui  sert à construire le dictionnaire d'une commande.
+// son nom, sa description, ses options etc
+
+module.exports = {
+  data: new SlashCommandBuilder.setName("ping").setDescription(
+    "Replies with Pong!"
+  ),
+
+  // execute est la fonction asychrone qui servira de handler. Quand l'utilisateur tapera "/ping"
+  // c'est execute qui sera appelée
+  async execute(interaction: typeof ApplicationCommand) {
+    interaction.reply("pong");
+  },
+};
